2e13a70e44be5a10c83e0d4e0487abb7ad416d74
Daniel B. Zhang <42197094+danielbzhang@users.noreply.github.com>
2022-04-27 10:10:19 -0400

    Update submission.js

--- submission.js
+++ submission.js
@@ -64,4 +64,22 @@ const removeParenth = function (str) {
 
 const scoreScrabble = function (str) {
   // your code here - don't forget to return a number!
+  const scores = {
+    'a':1, 'e':1, 'i':1, 'o':1, 'u':1, 'l':1, 'n':1, 'r':1, 's':1, 't':1,
+    'd':2, 'g':2,
+    'b':3, 'c':3, 'm':3, 'p':3,
+    'f':4, 'h':4, 'v':4, 'w':4, 'y':4,
+    'k':5,
+    'j':8, 'x':8,
+    'q':10, 'z':10
+  };
+
+  let sum = 0;
+  str = str.toLowerCase();
+
+  for (let i=0; i<str.length; i++) {
+    sum += scores[str[i]];
+  }
+  return sum;
+
 };
